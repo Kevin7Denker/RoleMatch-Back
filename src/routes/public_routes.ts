5f9577419cf1cac55ee718bf23acb451e95b2e34
Kevin Denker <oficialdenker@gmail.com>
2024-04-02 22:54:54 -0300

    Update public_routes.ts

--- src/routes/public_routes.ts
+++ src/routes/public_routes.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const route = express.Router();
 
-route.get("/", (request, response) => {
-  response.status(200).json("Bem Vindo ao RoleMatch Api");
+route.get("/", (req: Request, res: Response) => {
+  res.status(200).json("Bem Vindo ao RoleMatch Api");
 });
 
 export default route;
